1e086be0930a9dd699218b69a7c4eab4ae291346
darwendarwendarwen <darwin.dwin.luk@gmail.com>
2024-12-09 01:34:42 -0500

    updated c file

--- project-app/accelerometer-app/accelerometer-app.c
+++ project-app/accelerometer-app/accelerometer-app.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <linux/i2c-dev.h>
 
 
-#define I2C_BUS "/dev/i2d-5"
+#define I2C_BUS "/dev/i2c-5"
 #define DEVICE_ADDR 0x53
+#define READ_REG 0xA7
+#define WRITE_REG 0xA6
 
 
 int main() {
